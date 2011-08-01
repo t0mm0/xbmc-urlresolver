025ab80748113625cc902d23296d3d57d07769f8
t0mm0 <dev@onairsoftware.co.uk>
2011-08-01 17:32:36 +0100

    add sckshare and videoweed plugins

---
+++ script.module.urlresolver/lib/urlresolver/plugins/videoweed.py
@@ -0,0 +1,43 @@
+"""
+    urlresolver XBMC Addon
+    Copyright (C) 2011 t0mm0
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
+import urllib2
+from urlresolver.plugnplay.interfaces import UrlResolver
+from urlresolver.plugnplay.interfaces import PluginSettings
+from urlresolver.plugnplay import Plugin
+
+class VideoweedResolver(Plugin, UrlResolver, PluginSettings):
+    implements = [UrlResolver, PluginSettings]
+    name = "videoweed.es"
+
+    def __init__(self):
+        p = self.get_setting('priority') or 100
+        self.priority = int(p)
+
+    def get_media_url(self, web_url):
+        #grab stream address
+        response = urllib2.urlopen(web_url)
+        html = response.read()
+        stream_url = re.search('flashvars.file="(.+?)"', html).group(1)
+        return stream_url
+        
+    def valid_url(self, web_url):
+        return re.match('http:\/\/(?:www.)?videoweed.(?:es|com)\/file\/' + 
+                        '(?:[0-9a-z]+)(?:\/.+)?', web_url)
+
