e2ced0c9a417ee4baaf448b80dc125fd338d7a40
anarchintosh <j48antialias@gmail.com>
2011-09-13 16:19:59 +0100

    updated countdown script

--- script.module.urlresolver/lib/urlresolver/countdown.py
+++ script.module.urlresolver/lib/urlresolver/countdown.py
@@ -1,38 +1,45 @@
 '''
-Countdown XBMC 0.2
+Countdown XBMC 0.3
 Copyleft Anarchintosh
 
-Set a countdown dialog for XBMC.
+Set a countdown dialog with a progress bar for XBMC.
 Necessary for some filehosters eg. megaupload
 '''
 
 import xbmc, xbmcgui
 
-def countdown(time_to_wait,title='',text=''):
-    return do_xbmc_wait(time_to_wait,title,text)
+def countdown( time_to_wait, title='', text='' ):
 
-def do_xbmc_wait(time_to_wait,title,text):
-
-    print 'waiting '+str(time_to_wait)+' secs'
-
+    time_to_wait = int(time_to_wait)
+    
     pDialog = xbmcgui.DialogProgress()
     ret = pDialog.create(title)
 
+    print 'waiting '+str(time_to_wait)+' secs'
+    
     secs=0
     percent=0
     increment = 100 / time_to_wait
 
     cancelled = False
-    while secs < time_to_wait:
-        secs = secs + 1
-        percent = increment*secs
+    while secs <= time_to_wait:
+
+        percent = increment * secs
+
         secs_left = str((time_to_wait - secs))
+
+        if secs != 0: xbmc.sleep(1000)
+
+        if secs_left == '0': percent = 100
+        
         remaining_display = ' Wait '+secs_left+' seconds for the video stream to activate...'
         pDialog.update(percent,' '+text,remaining_display)
-        xbmc.sleep(1000)
+        
         if (pDialog.iscanceled()):
              cancelled = True
              break
+
+        secs += 1
 
     if cancelled == True:     
          print 'wait cancelled'
@@ -40,4 +47,3 @@ def do_xbmc_wait(time_to_wait,title,text):
     else:
          print 'done waiting'
          return True
-
